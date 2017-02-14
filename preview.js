0ff0997d233d2ca0f6f01fcc9d96bdb7678a2cf7
Erik <erikcui9020@gmail.com>
2017-02-13 18:08:42 -0800

    code now is able to insert html elements into text
    this took way too much digging to figure out omg

--- preview.js
+++ preview.js
@@ -56,6 +56,23 @@ function walk(node, regex) {
     }
 }
 
+// How to replace text with HTML element?
+// split? create element? we can't use textnode right? splitText?
+// splitText and normalize can be used to split text nodes and concat them
 function handleText(textNode) {
-    textNode.nodeValue = textNode.nodeValue.replace(regexDLsite, "TEST");
+    var text = textNode.nodeValue;
+    var textNodeMatches = text.match(regexDLsite);
+    if (textNodeMatches !== typeof "undefined" && textNodeMatches !== null){
+        var replacementNode = textNode.splitText(3); // stub
+        var btn = document.createElement("BUTTON"); // stub
+        btn.appendChild(document.createTextNode("TESTING")); // stub
+        textNode.parentNode.insertBefore(btn, replacementNode);
+    }
+
+    //var test = "<h1>test</h1>";
+    //textNode.nodeValue = textNode.nodeValue.replace(regexDLsite, test);
+}
+
+function getOffsetOfMatches(){
+    //stub
 }
